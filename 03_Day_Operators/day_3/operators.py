a87c0279775a3538b2a1de5cbf71ccfd12f6f560
tibahhsn <110685520+tibahhsn@users.noreply.github.com>
2026-04-04 19:29:41 +0200

    Update operators.py

--- 03_Day_Operators/day_3/operators.py
+++ 03_Day_Operators/day_3/operators.py
@@ -12,3 +12,15 @@ area = 0.5 * base * height
 area = int(area)
 area = str(area)
 print("The area of the triangle is " + area)23
+
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter = a + b + c
+print("The perimeter of the triangle is " + str(perimeter))
+
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter = a + b + c
+print("The perimeter of the triangle is " + str(perimeter))
