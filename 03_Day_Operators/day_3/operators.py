0cf6bf82cb66f305ea9d051cbda15900d46d4d56
tibahhsn <110685520+tibahhsn@users.noreply.github.com>
2026-04-04 18:34:38 +0200

    Create operators.py

---
+++ 03_Day_Operators/day_3/operators.py
@@ -0,0 +1,14 @@
+age = int(19)
+height = float(1.63)
+complex_number = complex(1j)
+
+print("Enter base: ")
+base = input()
+base = float(base)
+print("Enter height: ")
+height = input()
+height = float(height)
+area = 0.5 * base * height
+area = int(area)
+area = str(area)
+print("The area of the triangle is " + area)23
