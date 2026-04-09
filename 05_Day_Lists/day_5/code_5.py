53dc136a15ff6e913d5bcb645a8c4cf5dd3b0000
tibahhsn <110685520+tibahhsn@users.noreply.github.com>
2026-04-09 21:29:12 +0200

    Update and rename code_5 to code_5.py

---
+++ 05_Day_Lists/day_5/code_5.py
@@ -0,0 +1,17 @@
+# Declare an empty list (1)
+print("\n1. DECLARE AN EMPTY LIST")
+empty_list = list()
+print(f"Empty list: {empty_list}")
+print(f"Empty list type: {type(empty_list)}")
+
+# List with more than 5 items (2)
+print("\n2. LIST WITH MORE THAN 5 ITEMS")
+alien_stage = ["Mizi", "Sua", "Ivan", "Till", "Luka", "Hyuna"]
+print(f"Alien stage characters: {alien_stage}")
+
+# Find length (3)
+print("\n3. FIND LENGTH")
+print(f"Length of the list: {len(alien_stage)}")
+
+# Get first, middle, last item (4)
+print("\n4. GET FIRST, MIDDLE, LAST ITEM")
